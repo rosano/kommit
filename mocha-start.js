0b82721190cb0d6ce3d371fa9426cac2d24eb4dd
Rosano Coutinho <pub@xeari.com>
2020-03-26 16:36:47 -0300

    Template mocha-start

---
+++ mocha-start.js
@@ -0,0 +1,39 @@
+const KMTStorageModule = require('./os-app/_shared/KMTStorageModule/main.js');
+const KMTDocumentStorage = require('./os-app/_shared/KMTDocument/storage.js');
+
+(function KMTMochaStorage() {
+	if (process.env.OLSK_TESTING_BEHAVIOUR === 'true') {
+		return;
+	}
+
+	const uSerial = function (inputData) {
+		return inputData.reduce(async function (coll, e) {
+			return e.then(Array.prototype.concat.bind(await coll));
+		}, Promise.resolve([]));
+	};
+
+	before(function(done) {
+		global.KMTTestingStorageClient = require('./os-app/_shared/KMTStorageClient/main.js').KMTStorageClient({
+			modules: [
+				KMTStorageModule.KMTStorageModule([
+					KMTDocumentStorage.KMTDocumentStorage,
+				].map(function (e) {
+					return {
+						KMTCollectionStorageGenerator: e,
+						KMTCollectionChangeDelegate: null,
+					};
+				}))
+			],
+		});
+
+		done();
+	});
+
+	beforeEach(async function() {
+		await uSerial([
+			'kmt_documents',
+		].map(async function (e) {
+			return await Promise.all(Object.keys(await global.KMTTestingStorageClient.emojitimer[e].KMTStorageList()).map(global.KMTTestingStorageClient.emojitimer[e].KMTStorageDelete));
+		}));
+	});
+})();
