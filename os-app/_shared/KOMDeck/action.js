b213031d5a03fc76bbb74363b00ed5748d3e84ae
Rosano Coutinho <pub@xeari.com>
2020-04-10 11:30:26 -0300

    Tidy deck action

--- os-app/_shared/KOMDeck/action.js
+++ os-app/_shared/KOMDeck/action.js
@@ -2,38 +2,44 @@ import * as KOMDeckMetal from './metal.js';
 import { factory, detectPrng } from 'ulid';
 const uniqueID = typeof require === 'undefined' && navigator.appName === 'Zombie' ? factory(detectPrng(true)) : factory();
 
-export const KOMDeckActionCreate = async function(storageClient, inputData) {
-	if (typeof inputData !== 'object' || inputData === null) {
-		return Promise.reject(new Error('KOMErrorInputNotValid'));
-	}
+const mod = {
 
-	let creationDate = new Date();
+	 async KOMDeckActionCreate (storageClient, inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			return Promise.reject(new Error('KOMErrorInputNotValid'));
+		}
 
-	return await KOMDeckMetal.KOMDeckMetalWrite(storageClient, Object.assign(inputData, {
-		KOMDeckID: uniqueID(),
-		KOMDeckCreationDate: creationDate,
-		KOMDeckModificationDate: creationDate,
-	}));
+		let creationDate = new Date();
+
+		return await KOMDeckMetal.KOMDeckMetalWrite(storageClient, Object.assign(inputData, {
+			KOMDeckID: uniqueID(),
+			KOMDeckCreationDate: creationDate,
+			KOMDeckModificationDate: creationDate,
+		}));
+	},
+
+	 async KOMDeckActionRead (storageClient, inputData) {
+		return await KOMDeckMetal.KOMDeckMetalRead(storageClient, inputData);
+	},
+
+	 async KOMDeckActionUpdate (storageClient, inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			return Promise.reject(new Error('KOMErrorInputNotValid'));
+		}
+
+		return await KOMDeckMetal.KOMDeckMetalWrite(storageClient, Object.assign(inputData, {
+			KOMDeckModificationDate: new Date(),
+		}));
+	},
+
+	 async KOMDeckActionDelete (storageClient, inputData) {
+		return await KOMDeckMetal.KOMDeckMetalDelete(storageClient, inputData);
+	},
+
+	 async KOMDeckActionList (storageClient) {
+		return Object.values(await KOMDeckMetal.KOMDeckMetalList(storageClient));
+	},
+	
 };
-
-export const KOMDeckActionRead = async function(storageClient, inputData) {
-	return await KOMDeckMetal.KOMDeckMetalRead(storageClient, inputData);
-};
-
-export const KOMDeckActionUpdate = async function(storageClient, inputData) {
-	if (typeof inputData !== 'object' || inputData === null) {
-		return Promise.reject(new Error('KOMErrorInputNotValid'));
-	}
-
-	return await KOMDeckMetal.KOMDeckMetalWrite(storageClient, Object.assign(inputData, {
-		KOMDeckModificationDate: new Date(),
-	}));
-};
-
-export const KOMDeckActionDelete = async function(storageClient, inputData) {
-	return await KOMDeckMetal.KOMDeckMetalDelete(storageClient, inputData);
-};
-
-export const KOMDeckActionList = async function(storageClient) {
-	return Object.values(await KOMDeckMetal.KOMDeckMetalList(storageClient));
-};
+	
+export default mod;
