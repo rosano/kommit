7fae10602ead77bc05262fa4ac519258e1115904
Rosano Coutinho <pub@xeari.com>
2020-04-10 11:29:51 -0300

    Tidy card action

--- os-app/_shared/KOMCard/action.js
+++ os-app/_shared/KOMCard/action.js
@@ -2,38 +2,44 @@ import * as KOMCardMetal from './metal.js';
 import { factory, detectPrng } from 'ulid';
 const uniqueID = typeof require === 'undefined' && navigator.appName === 'Zombie' ? factory(detectPrng(true)) : factory();
 
-export const KOMCardActionCreate = async function(storageClient, inputData) {
-	if (typeof inputData !== 'object' || inputData === null) {
-		return Promise.reject(new Error('KOMErrorInputNotValid'));
-	}
+const mod = {
 
-	let creationDate = new Date();
+	 async KOMCardActionCreate (storageClient, inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			return Promise.reject(new Error('KOMErrorInputNotValid'));
+		}
 
-	return await KOMCardMetal.KOMCardMetalWrite(storageClient, Object.assign(inputData, {
-		KOMCardID: uniqueID(),
-		KOMCardCreationDate: creationDate,
-		KOMCardModificationDate: creationDate,
-	}));
+		let creationDate = new Date();
+
+		return await KOMCardMetal.KOMCardMetalWrite(storageClient, Object.assign(inputData, {
+			KOMCardID: uniqueID(),
+			KOMCardCreationDate: creationDate,
+			KOMCardModificationDate: creationDate,
+		}));
+	},
+
+	 async KOMCardActionRead (storageClient, inputData) {
+		return await KOMCardMetal.KOMCardMetalRead(storageClient, inputData);
+	},
+
+	 async KOMCardActionUpdate (storageClient, inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			return Promise.reject(new Error('KOMErrorInputNotValid'));
+		}
+
+		return await KOMCardMetal.KOMCardMetalWrite(storageClient, Object.assign(inputData, {
+			KOMCardModificationDate: new Date(),
+		}));
+	},
+
+	 async KOMCardActionDelete (storageClient, inputData) {
+		return await KOMCardMetal.KOMCardMetalDelete(storageClient, inputData);
+	},
+
+	 async KOMCardActionList (storageClient) {
+		return Object.values(await KOMCardMetal.KOMCardMetalList(storageClient));
+	},
+	
 };
-
-export const KOMCardActionRead = async function(storageClient, inputData) {
-	return await KOMCardMetal.KOMCardMetalRead(storageClient, inputData);
-};
-
-export const KOMCardActionUpdate = async function(storageClient, inputData) {
-	if (typeof inputData !== 'object' || inputData === null) {
-		return Promise.reject(new Error('KOMErrorInputNotValid'));
-	}
-
-	return await KOMCardMetal.KOMCardMetalWrite(storageClient, Object.assign(inputData, {
-		KOMCardModificationDate: new Date(),
-	}));
-};
-
-export const KOMCardActionDelete = async function(storageClient, inputData) {
-	return await KOMCardMetal.KOMCardMetalDelete(storageClient, inputData);
-};
-
-export const KOMCardActionList = async function(storageClient) {
-	return Object.values(await KOMCardMetal.KOMCardMetalList(storageClient));
-};
+	
+export default mod;
