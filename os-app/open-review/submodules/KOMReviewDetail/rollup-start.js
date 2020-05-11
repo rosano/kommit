f785b3fa69ca4d6772f211f3b5061511b036beed
Rosano Coutinho <pub@xeari.com>
2020-05-11 10:23:22 -0300

    Tidy

--- os-app/open-review/submodules/KOMReviewDetail/rollup-start.js
+++ os-app/open-review/submodules/KOMReviewDetail/rollup-start.js
@@ -6,24 +6,24 @@ import KOMSpacingModel from '../../../_shared/KOMSpacing/model.js';
 const KOMReviewDetail = new RollupStart({
 	target: document.body,
 	props: Object.assign({
-		KOMReviewDetailDispatchBack () {
+		KOMReviewDetailDispatchBack: (function _KOMReviewDetailDispatchBack () {
 			window.TestKOMReviewDetailDispatchBack.innerHTML = parseInt(window.TestKOMReviewDetailDispatchBack.innerHTML) + 1;
-		},
-		KOMReviewDetailDispatchDiscard (inputData) {
+		}),
+		KOMReviewDetailDispatchDiscard: (function _KOMReviewDetailDispatchDiscard (inputData) {
 			window.TestKOMReviewDetailDispatchDiscard.innerHTML = parseInt(window.TestKOMReviewDetailDispatchDiscard.innerHTML) + 1;
 			window.TestKOMReviewDetailDispatchDiscardData.innerHTML = JSON.stringify(inputData);
-		},
-		KOMReviewDetailDispatchRename (inputData) {
+		}),
+		KOMReviewDetailDispatchRename: (function _KOMReviewDetailDispatchRename (inputData) {
 			window.TestKOMReviewDetailDispatchRename.innerHTML = parseInt(window.TestKOMReviewDetailDispatchRename.innerHTML) + 1;
 			window.TestKOMReviewDetailDispatchRenameData.innerHTML = JSON.stringify(inputData);
-		},
-		KOMReviewDetailDispatchBrowse () {
+		}),
+		KOMReviewDetailDispatchBrowse: (function _KOMReviewDetailDispatchBrowse () {
 			window.TestKOMReviewDetailDispatchBrowse.innerHTML = parseInt(window.TestKOMReviewDetailDispatchBrowse.innerHTML) + 1;
-		},
-		KOMReviewDetailDispatchPlay (inputData) {
+		}),
+		KOMReviewDetailDispatchPlay: (function _KOMReviewDetailDispatchPlay (inputData) {
 			window.TestKOMReviewDetailDispatchPlay.innerHTML = parseInt(window.TestKOMReviewDetailDispatchPlay.innerHTML) + 1;
 			window.TestKOMReviewDetailDispatchPlayData.innerHTML = inputData;
-		},
+		}),
 	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function (e) {
 		if (['KOMReviewDetailDeck'].includes(e[0])) {
 			e[1] = JSON.parse(e[1]);
