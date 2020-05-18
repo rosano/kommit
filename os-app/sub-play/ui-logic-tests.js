2a35c7ab5b53d407c1dcd3a336f6a97d8499b8be
Rosano Coutinho <pub@xeari.com>
2020-05-18 10:49:46 -0300

    Spec learning_and_Easy

--- os-app/sub-play/ui-logic-tests.js
+++ os-app/sub-play/ui-logic-tests.js
@@ -1092,19 +1092,22 @@ describe('KOMPlayRespond', function test_KOMPlayRespond() {
 		
 		const spacing = kTesting.StubSpacingObjectValid();
 		const state = uState(spacing, [kTesting.StubSpacingObjectValid()]);
-		const chronicle = uChronicle({
+		let chronicle = uChronicle({
 			KOMChronicleResponseType: mainModule.KOMPlayResponseTypeGood(),
 		});
+		const events = [];
 
 		before(function () {
 			mainModule.KOMPlayRespond(state, chronicle);
+
+			events.push(uChronicle(chronicle));
 		});
 		
 		before(function () {
 			state.KOMPlayStateQueue.unshift(state.KOMPlayStateCurrent);
 			state.KOMPlayStateCurrent = state.KOMPlayStateWait.pop();
 
-			mainModule.KOMPlayRespond(state, Object.assign(chronicle, {
+			mainModule.KOMPlayRespond(state, chronicle = uChronicle({
 				KOMChronicleResponseDate: state.KOMPlayStateCurrent.KOMSpacingDueDate,
 				KOMChronicleResponseType: mainModule.KOMPlayResponseTypeEasy(),
 			}));	
@@ -1115,7 +1118,11 @@ describe('KOMPlayRespond', function test_KOMPlayRespond() {
 				KOMSpacingInterval: mainModule.KOMPlayResponseIntervalGraduateEasy(),
 				KOMSpacingMultiplier: mainModule.KOMPlayResponseMultiplierDefault(),
 				KOMSpacingDueDate: new Date(chronicle.KOMChronicleResponseDate.valueOf() + 1000 * 60 * 60 * 24 * mainModule.KOMPlayResponseIntervalGraduateEasy()),
-				KOMSpacingChronicles: [chronicle, chronicle],
+				KOMSpacingChronicles: events.concat(uChronicle({
+					KOMChronicleResponseDate: chronicle.KOMChronicleResponseDate,
+					KOMChronicleResponseType: chronicle.KOMChronicleResponseType,
+					KOMChronicleDueDate: spacing.KOMSpacingDueDate,
+				})),
 			}));
 		});
 		
