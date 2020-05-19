261b454cda0d9004466fbdd63cb7910d852ea599
Rosano Coutinho <pub@xeari.com>
2020-05-19 11:12:37 -0300

    Define link guard middlewares

--- os-app/_shared/common/controller.js
+++ os-app/_shared/common/controller.js
@@ -1,3 +1,14 @@
+exports.OLSKControllerSharedMiddlewares = function() {
+	return {
+		KOMSharedDonateLinkGuardMiddleware (req, res, next) {
+			return next(require('./logic.js').KOMSharedDonateLinkGuard(process.env));
+		},
+		KOMSharedGitHubLinkGuardMiddleware (req, res, next) {
+			return next(require('./logic.js').KOMSharedGitHubLinkGuard(process.env));
+		},
+	};
+};
+
 exports.OLSKControllerSharedStaticAssetFolders = function() {
 	return [
 		'_shared/__external',
