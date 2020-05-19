e09b6120c68684027b9ccc53228c61aeed2cfa09
Rosano Coutinho <pub@xeari.com>
2020-05-19 11:12:18 -0300

    Spec url guards

---
+++ os-app/_shared/common/logic.js
@@ -0,0 +1,25 @@
+const mod = {
+
+	KOMSharedDonateLinkGuard (inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('KOMErrorInputNotValid');
+		}
+
+		if (!(inputData.KOM_SHARED_DONATE_URL || '').trim()) {
+			return new Error('KOM_SHARED_DONATE_URL not defined');
+		}
+	},
+
+	KOMSharedGitHubLinkGuard (inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('KOMErrorInputNotValid');
+		}
+
+		if (!(inputData.KOM_SHARED_GITHUB_URL || '').trim()) {
+			return new Error('KOM_SHARED_GITHUB_URL not defined');
+		}
+	},
+
+};
+
+Object.assign(exports, mod);
