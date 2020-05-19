2ba0c03c3df9d6c46647caefdb61c97ec11493a0
Rosano Coutinho <pub@xeari.com>
2020-05-19 11:29:02 -0300

    Spec KOMVitrineRouteGuard

---
+++ os-app/open-vitrine/logic-tests.js
@@ -0,0 +1,31 @@
+const { throws, deepEqual } = require('assert');
+
+const mainModule = require('./logic.js');
+
+describe('KOMVitrineRouteGuard', function test_KOMVitrineRouteGuard() {
+
+	const StubEnvValid = function () {
+		return {
+			KOM_VITRINE_ANKI_URL: 'alfa',
+		};
+	};
+
+	it('throws if not object', function() {
+		throws(function() {
+			mainModule.KOMVitrineRouteGuard(null);
+		}, /KOMErrorInputNotValid/);
+	});
+
+	it('returns error if no KOM_VITRINE_ANKI_URL', function () {
+		deepEqual(mainModule.KOMVitrineRouteGuard(Object.assign(StubEnvValid(), {
+			KOM_VITRINE_ANKI_URL: null,
+		})), new Error('KOM_VITRINE_ANKI_URL not defined'));
+	});
+
+	it('returns error if KOM_VITRINE_ANKI_URL blank', function () {
+		deepEqual(mainModule.KOMVitrineRouteGuard(Object.assign(StubEnvValid(), {
+			KOM_VITRINE_ANKI_URL: ' ',
+		})), new Error('KOM_VITRINE_ANKI_URL not defined'));
+	});
+
+});
---
+++ os-app/open-vitrine/logic.js
@@ -0,0 +1,15 @@
+const mod = {
+
+	KOMVitrineRouteGuard (inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('KOMErrorInputNotValid');
+		}
+
+		if (!(inputData.KOM_VITRINE_ANKI_URL || '').trim()) {
+			return new Error('KOM_VITRINE_ANKI_URL not defined');
+		}
+	},
+
+};
+
+Object.assign(exports, mod)
