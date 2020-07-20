2d564137ef2cebcf103a7d39415db22ad6385d4b
Rosano Coutinho <pub@xeari.com>
2020-07-20 12:33:58 -0300

    Fix typo

--- os-app/sub-browse/submodules/KOMBrowseInfo/main.svelte
+++ os-app/sub-browse/submodules/KOMBrowseInfo/main.svelte
@@ -29,7 +29,7 @@ const mod = {
 			return;
 		}
 
-		if (KOMBrowseInfoItem.KOMCardTags.includes(inputData)) {
+		if ((KOMBrowseInfoItem.KOMCardTags || []).includes(inputData)) {
 			return;
 		}
 		
