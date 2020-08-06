4528a2d226a34db200674fc95e501325fa73e1a7
Rosano Coutinho <pub@xeari.com>
2020-08-06 20:20:08 -0300

    Fix typo

--- os-app/open-review/submodules/KOMReviewChartCompositionCollection/rollup-start.js
+++ os-app/open-review/submodules/KOMReviewChartCompositionCollection/rollup-start.js
@@ -3,7 +3,7 @@ import RollupStart from './main.svelte';
 const KOMReviewChartCompositionCollection = new RollupStart({
 	target: document.body,
 	props: Object.assign({
-		KOMReviewChartElementHorizontalStackedBarColors: ['alfa', 'bravo', 'charlie'],
+		KOMReviewChartElementHorizontalStackedBarColors: ['alfa', 'bravo', 'charlie', 'delta'],
 	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function (e) {
 		if (['KOMReviewChartCompositionCollectionData'].includes(e[0])) {
 			e[1] = JSON.parse(e[1]);
