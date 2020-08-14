fe8b61039b4467e19a8863741df0cd1015b5d7a9
Rosano Coutinho <pub@xeari.com>
2020-08-14 17:23:44 -0300

    Update service worker API

--- os-app/tech-sw/controller.js
+++ os-app/tech-sw/controller.js
@@ -1,4 +1,4 @@
-const kKOMServiceWorkerVersionID = process.env.HEROKU_SLUG_COMMIT || Date.now();
+const kKOMServiceWorkerVersionID = process.env.HEROKU_SLUG_COMMIT || Date.now().toString();
 
 const OLSKServiceWorker = require('../_shared/__external/OLSKServiceWorker/main.js');
 
@@ -9,8 +9,8 @@ exports.OLSKControllerRoutes = function () {
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction(req, res, next) {
 				return res.type('js').send(OLSKServiceWorker.OLSKServiceWorkerView({
-					VERSION_ID_TOKEN: kKOMServiceWorkerVersionID.toString(),
-					REFERRER_MATCH_TOKEN: res.locals.OLSKCanonicalFor('KOMReviewRoute').replace(/\//g, '\\/'),
+					VERSION_ID_TOKEN: kKOMServiceWorkerVersionID,
+					ORIGIN_PAGE_PATH_TOKEN: res.locals.OLSKCanonicalFor('KOMReviewRoute'),
 				}));
 			},
 		},
