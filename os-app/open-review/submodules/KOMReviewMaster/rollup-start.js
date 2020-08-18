a577af77288a12522bca2b36c9b72d483eaa3201
Rosano Coutinho <pub@xeari.com>
2020-08-18 12:36:43 -0300

    Tidy

--- os-app/open-review/submodules/KOMReviewMaster/rollup-start.js
+++ os-app/open-review/submodules/KOMReviewMaster/rollup-start.js
@@ -7,14 +7,14 @@ const KOMReviewMaster = new RollupStart({
 	target: document.body,
 	props: Object.assign({
 		KOMReviewMasterItems: [],
-		KOMReviewMasterDispatchCreate(inputData) {
+		KOMReviewMasterDispatchCreate: (function _KOMReviewMasterDispatchCreate(inputData) {
 			window.TestKOMReviewMasterDispatchCreate.innerHTML = parseInt(window.TestKOMReviewMasterDispatchCreate.innerHTML) + 1;
 			window.TestKOMReviewMasterDispatchCreateData.innerHTML = inputData;
-		},
-		KOMReviewMasterDispatchSelect(inputData) {
+		}),
+		KOMReviewMasterDispatchSelect: (function _KOMReviewMasterDispatchSelect(inputData) {
 			window.TestKOMReviewMasterDispatchSelect.innerHTML = parseInt(window.TestKOMReviewMasterDispatchSelect.innerHTML) + 1;
 			window.TestKOMReviewMasterDispatchSelectData.innerHTML = JSON.stringify(inputData);
-		},
+		}),
 	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function (e) {
 		if (['KOMReviewMasterItems'].includes(e[0])) {
 			e[1] = JSON.parse(e[1]).map(function (e) {
