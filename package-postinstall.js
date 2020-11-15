3f16c7670cc0783cab1436194140a022ef40e928
Rosano Coutinho <pub@xeari.com>
2020-11-15 14:14:23 -0300

    Extract OLSKPostinstallHotfix

--- package-postinstall.js
+++ package-postinstall.js
@@ -1,64 +1,18 @@
-(function OLSKPostinstallPatchZombieForUITests() {
+(function OLSKPostinstallHotfix() {
 	if (process.env.NODE_ENV === 'production') {
 		return;
 	}
 
-	let filePath = './node_modules/zombie/lib/document.js';
-
-	require('fs').writeFileSync(filePath, require('OLSKString').OLSKStringPatch(
-		require('fs').readFileSync(filePath, 'utf8'),
-		'this.dispatchEvent(event);',
-		`this.dispatchEvent(event)
-			const handled = browser.emit('OLSKMessage', data);
-		  if (!handled)
-		      browser.log('Unhandled message("%s")');`
-	));
-})();
-
-(function ROCOHotfixULIDForBrowserTesting() {
-	if (process.env.NODE_ENV === 'production') {
-		return;
-	}
-
-	let filePath = './node_modules/ulid/dist/index.esm.js';
-	require('fs').writeFileSync(filePath, require('fs')
-		.readFileSync(filePath, 'utf8')
-		.replace(
-			'console.error("secure crypto unusable, falling back to insecure Math.random()!");',
-			'// console.error("secure crypto unusable, falling back to insecure Math.random()!");')
-		.replace(
-			'var ulid = factory();',
-			'// var ulid = factory();')
-		.replace(
-			'export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory, ulid };',
-			'export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory };')
-	);
-})();
-
-(function ROCOHotfixLaunchletForSkipWait() {
-	if (process.env.NODE_ENV === 'production') {
-		return;
-	}
-
-	const filePath = './node_modules/launchlet/__compiled/launchlet.js';
-	require('fs').writeFileSync(filePath, require('OLSKString').OLSKStringPatch(
-		require('fs').readFileSync(filePath, 'utf8'),
-		',100',
-		',1',
-	));
-})();
-
-(function ROCOHotfixRemoteStorageForClearHash() {
-	if (process.env.NODE_ENV === 'production') {
-		return;
-	}
-
-	const filePath = './node_modules/remotestoragejs/release/remotestorage.js';
-	require('fs').writeFileSync(filePath, require('OLSKString').OLSKStringPatch(
-		require('fs').readFileSync(filePath, 'utf8'),
-		'r&&((t=u.getLocation()).hash',
-		'r.access_token&&((t=u.getLocation()).hash',
-	));
+	Object.entries(require('OLSKHotfix').OLSKHotfixPatches()).forEach(function ([path, patches]) {
+		if (!require('fs').existsSync(path)) {
+			return;
+		}
+		
+		Object.entries(patches).forEach(function ([search, replace]) {
+			require('fs').writeFileSync(path, require('OLSKString').OLSKStringPatch(
+				require('fs').readFileSync(path, 'utf8'), search, replace));
+		});
+	});
 })();
 
 (function OLSKPostinstallExternalAssets() {
