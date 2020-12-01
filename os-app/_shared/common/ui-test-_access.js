c02c83da3888f4375ff5fdbed7841a7ed3c03aa5
Rosano Coutinho <pub@xeari.com>
2020-12-01 20:42:49 -0300

    Use browser.deepEqual in ui tests

--- os-app/_shared/common/ui-test-_access.js
+++ os-app/_shared/common/ui-test-_access.js
@@ -1,9 +1,7 @@
-import { deepEqual } from 'assert';
-
 describe('KOMCommon_Access', function test_KOMCommon_Access() {
 
 	it('redirects KOMCommonIdentityRedirect', async function () {
-		deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonical(require('./controller.js').OLSKControllerRoutes().KOMCommonIdentityRedirect))).text()).slice(0, 10), '<?xml vers');
+		browser.assert.deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonical(require('./controller.js').OLSKControllerRoutes().KOMCommonIdentityRedirect))).text()).slice(0, 10), '<?xml vers');
 	});
 
 });
