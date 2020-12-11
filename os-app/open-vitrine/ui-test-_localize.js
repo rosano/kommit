29f66a244e2465f8b4640449998435ede99aa654
Rosano Coutinho <pub@xeari.com>
2020-12-11 13:25:33 -0300

    Fix typo

--- os-app/open-vitrine/ui-test-_localize.js
+++ os-app/open-vitrine/ui-test-_localize.js
@@ -32,7 +32,7 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 
 		it('localizes KOMVitrineContent', function () {
 			const item = require('OLSKString').OLSKStringReplaceTokens(require('fs').readFileSync(require('path').join(__dirname, `text.${ OLSKRoutingLanguage }.md`), 'utf-8'), {
-				'_': '',
+				'\\*': '',
 				'\n\n': '\n',
 				'KOMVitrineDescription': uLocalized('KOMVitrineDescription'),
 			});
