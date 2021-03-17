9abdba83a09ea51a4ba86f3ee9beea8936c6c12b
Rosano Coutinho <pub@xeari.com>
2021-03-17 10:55:19 -0300

    Spec OLSKGazetteProject

--- os-app/open-vitrine/ui-test-_misc.js
+++ os-app/open-vitrine/ui-test-_misc.js
@@ -86,4 +86,12 @@ describe('KOMVitrine_Misc', function () {
 
 	});
 
+	describe('OLSKGazette', function test_OLSKGazette () {
+
+		it('sets src', function () {
+			browser.assert.attribute('.OLSKGazetteProjectField', 'value', 'RP_004');
+		});
+
+	});
+
 });
