9fa70b314f5204ec721f1d61305924a73ada2bcb
Rosano Coutinho <pub@xeari.com>
2021-04-17 10:01:16 -0300

    Purge ParamWindow

--- os-app/open-review/main.svelte
+++ os-app/open-review/main.svelte
@@ -169,7 +169,6 @@ const mod = {
 		}
 
 		items.push(...OLSKFund.OLSKFundRecipes({
-			ParamWindow: window,
 			OLSKLocalized: OLSKLocalized,
 			ParamConnected: !!mod._ValueCloudIdentity,
 			ParamAuthorized: !!mod._ValueFundClue,
@@ -185,7 +184,6 @@ const mod = {
 		}));
 
 		items.push(...OLSKTransport.OLSKTransportRecipes({
-			ParamWindow: window,
 			OLSKLocalized: OLSKLocalized,
 			OLSKTransportDispatchImportJSON: mod.OLSKTransportDispatchImportJSON,
 			OLSKTransportDispatchExportInput: mod.OLSKTransportDispatchExportInput,
@@ -194,7 +192,6 @@ const mod = {
 
 		if (mod._ValueZDRWrap.ZDRStorageProtocol === zerodatawrap.ZDRProtocolRemoteStorage()) {
 			items.push(...OLSKRemoteStorage.OLSKRemoteStorageRecipes({
-				ParamWindow: window,
 				ParamStorage: mod._ValueZDRWrap.ZDRStorageClient(),
 				OLSKLocalized: OLSKLocalized,
 				ParamMod: mod,
@@ -814,7 +811,6 @@ const mod = {
 				ParamLanguageCodes: window.OLSKPublicConstants('OLSKSharedPageLanguagesAvailable'),
 				ParamCurrentLanguage: window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage'),
 				ParamSpecUI: OLSK_SPEC_UI(),
-				ParamWindow: window,
 				ParamRouteConstant: window.OLSKPublicConstants('OLSKSharedActiveRouteConstant'),
 				OLSKCanonical: window.OLSKCanonical,
 			}).map(function (e) {
@@ -849,7 +845,6 @@ const mod = {
 		mod._OLSKWebView.modPublic.OLSKModalViewShow();
 
 		OLSKFund.OLSKFundListen({
-			ParamWindow: window,
 			OLSKFundDispatchReceive: mod.OLSKFundDispatchReceive,
 		});
 	},
@@ -1190,7 +1185,6 @@ const mod = {
 		mod._ValueFundClue = await mod.DataSettingValue('KOMSettingFundClue');
 
 		await OLSKFund.OLSKFundSetupPostPay({
-			ParamWindow: window,
 			ParamExistingClue: mod._ValueFundClue || null,
 			OLSKFundDispatchPersist: mod.OLSKFundDispatchPersist,
 		});
@@ -1206,7 +1200,6 @@ const mod = {
 		const item = {
 			OLSK_CRYPTO_PAIR_RECEIVER_PRIVATE: `OLSK_CRYPTO_PAIR_RECEIVER_PRIVATE_SWAP_TOKEN${ '' }`, // #purge
 			OLSK_CRYPTO_PAIR_SENDER_PUBLIC: 'OLSK_CRYPTO_PAIR_SENDER_PUBLIC_SWAP_TOKEN',
-			ParamWindow: window,
 			OLSK_FUND_API_URL: 'OLSK_FUND_API_URL_SWAP_TOKEN',
 			ParamBody: {
 				OLSKPactAuthType: mod._ValueZDRWrap.ZDRStorageProtocol === zerodatawrap.ZDRProtocolRemoteStorage() ? OLSKPact.OLSKPactAuthTypeRemoteStorage() : OLSKPact.OLSKPactAuthTypeFission(),
