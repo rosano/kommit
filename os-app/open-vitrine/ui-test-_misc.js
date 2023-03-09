4910e4f1fba6332dccb1709bbc7ef150b1ba070b
Rosano Coutinho <pub@xeari.com>
2023-03-09 14:53:35 +0700

    Use project name in gazette

--- os-app/open-vitrine/ui-test-_misc.js
+++ os-app/open-vitrine/ui-test-_misc.js
@@ -113,7 +113,7 @@ describe('KOMVitrine_Misc', function () {
 	describe('ROCOGazette', function test_ROCOGazette () {
 
 		it('sets ROCOBulletinProject', function () {
-			browser.assert.attribute('.ROCOBulletinProjectField', 'value', process.env.ROCO_SHARED_PROJECT_ID);
+			browser.assert.attribute('.ROCOBulletinProjectField', 'value', 'Kommit');
 		});
 
 	});
