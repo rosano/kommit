92608eddea2279ba3f0e00a96decec41d22c649a
Rosano Coutinho <github.com@xeari.com>
2025-09-09 10:27:24 +0100

    fission offline

--- package-postinstall.js
+++ package-postinstall.js
@@ -24,6 +24,5 @@
 		'OLSKUIAssets',
 		'ROCORootLink',
 		'vmsg',
-		'webnative',
 	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, 'os-app/_shared/__external'));
 })();
